41a32a23efc7cbf1c735e3edaecb3d46a1ef33d2
Hikari <158005693+webHikari@users.noreply.github.com>
2024-02-05 16:34:51 +0300

    again

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Устанавливаем папку 'public' в качестве статической папки
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Роутер для выдачи файлов
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/gogo', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+// Порт, на котором будет запущен сервер
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
