9e3a65514b051854bc42361f5ee6540a0f4c72b4
sunskycloud <q5503212@gmail.com>
2024-02-07 17:15:30 +0300

    +dumb activity control

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs')
+const dotenv = require('dotenv').config()
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Устанавливаем папку 'public' в качестве статической папки
+const observeActivity = (page) => {
+    const data = fs.readFile("./activity/" + page + ".txt",
+    (error, data) => {
+        if(error) {
+            return console.log(error);
+        }
+        data++;
+        fs.writeFileSync("./activity/" + page + ".txt", data);
+        console.log(page +": " +  data);
+    });
+}
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Роутер для выдачи файлов
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-app.get('/auth', (req, res) => {
+// Роутер для выдачи файлов на клиент
+app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    observeActivity('index')
 });
-// Порт, на котором будет запущен сервер
-const PORT = 3000;
+app.get('/rules', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'rules.html'));
+    observeActivity('rules')
+});
+app.get('/social', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'social.html'));
+    observeActivity('social')
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+    observeActivity('about')
+});
+app.get('/donate', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'donate.html'));
+    observeActivity('donate')
+});
+// API Роутер
+app.post('/api', (req, res) => {
+    res.send('hello')
+})
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
+    console.log(`Server dancin' on ${PORT} port`);
 });
