c114536685c269c4e21fc379903308379f1bd706
sunskycloud <q5503212@gmail.com>
2024-02-08 15:33:28 +0300

    + Rules page

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const observeActivity = (page) => {
+const observeActivity = async (page) => {
     const data = fs.readFile("./activity/" + page + ".txt",
     (error, data) => {
         if(error) {
